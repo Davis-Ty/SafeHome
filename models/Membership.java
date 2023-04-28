e99380d491f88facd1f211fe921a694ef82980b4
Tyon Davis <55100272+Davis-Ty@users.noreply.github.com>
2023-04-28 16:39:46 -0400

    Add files via upload

---
+++ models/Membership.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.io.Serializable;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import util.OracleConnection;
+
+/**
+ *
+ * @author Tyon Davis
+ */
+public class Membership implements Serializable {
+    private String membershipID;
+    private String fName;
+    private String lName;
+    private String email;
+
+    //define DB connection
+    private Connection conn = null;
+    
+    public String getMembershipID() {
+        return membershipID;
+    }
+
+    public void setMembershipID(String membershipID) {
+        this.membershipID = membershipID;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean login(String email, String pwd){
+        boolean validated = false;
+        try{
+            conn = OracleConnection.getConnection();
+            String sql = "select * from membership where "
+                    + " email =? and password = ?";
+            //create statement
+            PreparedStatement stmt
+                    = conn.prepareStatement(sql);
+            stmt.setString(1, email);
+            stmt.setString(2, pwd);
+            //run SQL
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+                //combination of email/pwd is correct
+                validated = true;
+            }
+        }catch(Exception exp){
+            exp.printStackTrace();
+        }finally{
+            conn = null;
+            OracleConnection.closeConnection();
+        }
+        
+        return validated;
+    } //login method ends
+    
+    public List<Membership> getMembers(){
+        List<Membership> mems = new ArrayList<Membership>();
+        try{
+            conn = OracleConnection.getConnection();
+            String sql = "select email, fname, lname from membership";
+            Statement stmt = conn.createStatement();
+            //Run SQL
+            ResultSet rs = stmt.executeQuery(sql);
+            //Process resultset
+            while(rs.next()){
+                //pull data in resultset //get email
+                Membership member = new Membership();
+                member.setEmail(rs.getString(1)); //email
+                member.setfName(rs.getString(2)); //First name
+                member.setlName(rs.getString(3));  //lastname
+                mems.add(member);
+            }
+        }catch(Exception exp){
+           exp.printStackTrace();
+        }finally{
+           conn = null;
+           OracleConnection.closeConnection();
+        }
+        return mems;
+    }//end getMembers()
+    
+    //test functions in Membership
+    public static void main(String[] args){
+        String email = "jdoe@clayton.edu";
+        String password = "0k123";
+        
+        Membership mem = new Membership();
+        if( mem.login(email, password) ){
+            System.out.println(email + 
+                    " Welcome to CSCI3320");
+        }
+        else {
+            System.out.println(email + 
+                    " does not has access!!!");
+        }
+        
+    }
+}
---
+++ util/OracleConnection.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Tyon Davis
+ */
+public class OracleConnection {
+    private static Connection connection = null;
+    
+    public static Connection getConnection(){
+        try{
+            System.out.println("Connecting to Oracle...");
+            //load jdbc driver class
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            //create connection object
+            connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@168.28.42.148:1521:csit", 
+                    "tdavis112", ///use your oracle user name
+                    "tdavis112"); //use your oracle password
+        }catch(Exception exps){
+            exps.printStackTrace();
+            return null;
+        }
+        return connection;
+    }
+    
+    public static void closeConnection(){
+        try{
+            //close connection
+            connection.close();
+        }catch(Exception exps){
+            exps.printStackTrace();
+        }
+    }
+    //build main function to test it
+    public static void main(String[] args){
+        try{
+            /*
+            Step 1: get oracle connection handle
+            step 2: build & Run SQL cmmd and statment
+            step 3: process returned results from DB
+            */
+            //Connection
+            Connection conn = OracleConnection.getConnection();
+            //Build SQL
+            String sql = "select sysdate from dual";
+            //execute
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            //Process results
+            while(rs.next()){
+              //pull data in resultset
+              System.out.println("Current Oralce Server time is:"
+                + rs.getString(1));
+            }
+        }catch(Exception exp){
+            exp.printStackTrace();                    
+        }finally{
+            OracleConnection.closeConnection();
+        }
+        
+    }
+}
